aafde0c45d70a7e843a7355a8b8ef807549d2b93
MattHew Phraxayavong <mphraxa@gmail.com>
2019-06-19 10:02:59 -0700

    palindromes complete

--- Code/palindromes.py
+++ Code/palindromes.py
@@ -12,23 +12,30 @@ def is_palindrome(text):
     backwards, ignoring punctuation, whitespace, and letter casing."""
     # implement is_palindrome_iterative and is_palindrome_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    print('hi')
-    reverse_text = text[::-1]
-    if reverse_text == text:
-        return True
-    return False
-    # assert isinstance(text, str), 'input is not a string: {}'.format(text)
-    # return is_palindrome_iterative(text)
-    # return is_palindrome_recursive(text)
 
-
-
+    assert isinstance(text, str), 'input is not a string: {}'.format(text)
+    return is_palindrome_iterative(text)
+    #return is_palindrome_recursive(text)
 
 def is_palindrome_iterative(text):
     # TODO: implement the is_palindrome function iteratively here
-    pass
     # once implemented, change is_palindrome to call is_palindrome_iterative
     # to verify that your iterative implementation passes all tests
+    left_index = 0
+    right_index = len(text)-1
+    # While our left index is less than our right index. It moves the pointers until they meet in the middle
+    while left_index < right_index:
+        # We check to see if our first and last index values are the same.
+        if text[left_index].lower() == text[right_index].lower():
+            #If it is, then we have a possible palindrome. We move each index to the next adjecent index.
+            #Our left index will increase by 1 in order to move to the index to the right
+            #Our right index will decrease by 1 in order to move to the index to the left
+            right_index -= 1
+            left_index += 1
+        else:
+            return False
+
+    return True
 
 
 def is_palindrome_recursive(text, left=None, right=None):
@@ -36,6 +43,18 @@ def is_palindrome_recursive(text, left=None, right=None):
     pass
     # once implemented, change is_palindrome to call is_palindrome_recursive
     # to verify that your iterative implementation passes all tests
+
+    if left is None and right is None:
+        left = 0
+        right = len(text)-1
+
+    if left < right:
+        if text[left] == text[right]:
+            return is_palindrome_recursive(text, left+1, right-1)
+        else:
+            return False
+    else:
+        return True
 
 
 def main():
