8a667c3e6872ccc686935f8ae069ff20d4091919
MattHew Phraxayavong <mphraxa@gmail.com>
2019-06-20 10:03:50 -0700

    strings completed

--- Code/strings.py
+++ Code/strings.py
@@ -5,13 +5,11 @@ def contains(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
-    current_index = 0
-    index_tracker = text[current_index]
-    pattern_tracker = pattern[current_index]
 
-
-    for index in text:
-        if
+    if pattern in text:
+        return True
+    else:
+        return False
 
 
 
@@ -22,6 +20,34 @@ def find_index(text, pattern):
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
 
+    # create a variable to keep track of our index
+    text_index = 0
+    # if our pattern is empty, we will return our text_index, which is defaulted to be set at 0
+    if pattern == '':
+        return text_index
+
+    # while our text_index, which starts at 0, is not equal to the length of our text, the code will run until our
+    # text_index and the length of the text we are passing in are the same.
+    while text_index != len(text):
+        """ We are iterating through our pattern. For example, if the pattern is 'abc', 'i' will become 'a' and then loop through
+            the whole if statements below it. Once the if statements have been completed, 'i' then becomes 'b' and it loops again
+            through the whole if statements.
+        """
+        for i in range(len(pattern)):
+            #if our text_index, which is currently at [0] plus our 'i', which starts at [0] is less than the length of our text
+            #For example, it will be 0 + 0, followed by 0 + 1, then 0 + 2, until it is no longer less than the length of our text
+            if text_index + i < len(text):
+                # The first character(index 0) of both the text and the pattern need to match
+                # For example, text is = 'abc' and pattern is = 'ab'
+                # First iteration will be value at index 0, this is because text[0 + 0] will give you text[0] and unwraps the value
+                # so it is if 'a' is not equal to 'a' break the loop
+                if text[text_index + i] != pattern[i]:
+                    break
+                if i == len(pattern) - 1:
+                    return text_index
+        text_index += 1
+
+
 
 def find_all_indexes(text, pattern):
     """Return a list of starting indexes of all occurrences of pattern in text,
@@ -29,7 +55,13 @@ def find_all_indexes(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+    indices = []
+    for index, _ in enumerate(text):
+        if pattern == text[index: (index + len(pattern))]:
+            print('find_all_index is:' + text[index: (index + len(pattern))])
+            indices.append(index)
 
+    return indices
 
 def test_string_algorithms(text, pattern):
     found = contains(text, pattern)
